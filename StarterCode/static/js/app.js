fbab026f1ea1ec755a0f7b7cdfb44bfd3775043f
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-08-26 09:19:34 -0700

    feat: Add basic functionality to UFO app according to specs

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,29 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+function addTd(curr_row, key, obj) {
+    curr_row.append("td").text(obj[key])
+}
+
+d3.select("#filter-btn").on("click", function () {
+    d3.event.preventDefault();
+
+    let filter_date =  d3.select("#datetime").property("value")
+    console.log(filter_date)
+
+    let results = data.filter(a => a.datetime === filter_date)
+    let table_body = d3.select("tbody")
+
+    for (let i = 0; i<results.length; i++) {
+        let curr_row = table_body.append("tr")
+        let obj = results[i]
+        addTd(curr_row, "datetime", obj)
+        addTd(curr_row, "city", obj)
+        addTd(curr_row, "state", obj)
+        addTd(curr_row, "country", obj)
+        addTd(curr_row, "shape", obj)
+        addTd(curr_row, "durationMinutes", obj)
+        addTd(curr_row, "comments", obj)
+    }
+})
